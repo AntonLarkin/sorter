d97aadff05e67652ef959cf67b0ce4715bb002f5
Anton <anton11061998@gmail.com>
2018-02-24 18:42:36 +0300

    Add project

--- src/index.js
+++ src/index.js
@@ -1,26 +1,48 @@
 class Sorter {
   constructor() {
     // your implementation
+    this.main=[];
+
   }
 
   add(element) {
     // your implementation
+    this.main.push(element);
   }
 
   at(index) {
     // your implementation
+return this.main[index];
   }
 
   get length() {
     // your implementation
+    return this.main.length
   }
 
   toArray() {
     // your implementation
+    return this.main;
   }
 
   sort(indices) {
     // your implementation
+   var main=this.main;
+   var copy=[];
+   for(var i=0;i<main.length;i++){
+       for(var j=0;j<indices.length;j++){
+        if(indices[j]==i){
+               copy.push(main[i]);
+        }
+       }
+   }
+
+  copy.sort();
+   for (var i=0; i<indices.length;i++){
+    main[indices[i]]=copy[i];
+}
+return main;
+
   }
 
   setComparator(compareFunction) {
